002211ee94013c1cdb1ec328e126e378e6b0e73d
Alberto Rocha <betogrun@gmail.com>
2018-02-18 15:46:48 -0300

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from './forms/forms.module';
 import { AnswersModule } from './answers/answers.module';
+import { RouterModule } from '@angular/router';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { AnswersModule } from './answers/answers.module';
     ColorPickerModule,
     ChartsModule,
     FormsModule,
-    AnswersModule
+    AnswersModule,
+    RouterModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
